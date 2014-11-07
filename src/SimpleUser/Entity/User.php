1826300584422ec6856d0ebdc0d78b0ed097febf
Maxime MANGÉ <maxime.mange@gmail.com>
2014-11-07 15:37:28 +0100

    Update User.php

--- src/SimpleUser/Entity/User.php
+++ src/SimpleUser/Entity/User.php
@@ -4,7 +4,7 @@ namespace SimpleUser\Entity;
 
 use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection();
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @author Thibault Clerice
